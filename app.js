731929f4c963ae480f5a1e2fd7686a37edb99920
adarsh kiratkudave <sangharsharanpise408@gmail.com>
2020-07-14 10:03:47 +0530

    added js files

--- app.js
+++ app.js
@@ -7,10 +7,10 @@
         var router = new Router([
             new Route('home', 'QuiverENHome.html', true),            
             new Route('quiverIt', 'QuiverIt.html'),
-            // new Route('features', 'features.html'),            
-            // new Route('support', 'support.html'),
-            // new Route('contact','contact.html'),
-            // new Route('package','Package.html')
+            new Route('features', 'Features.html'),            
+            new Route('support', 'support.html'),
+            new Route('contact','Contact.html'),
+            new Route('package','Package.html')
         ]);
     }
     init();
